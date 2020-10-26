54d2e76a404ded378eda8bd74d2ced5eee912068
Oscar Díaz <odiaz@synergytpc.com>
2020-10-26 15:32:18 +0000

    Added IOTDPS.tf

---
+++ Terraform/IOTDPS.tf
@@ -0,0 +1,34 @@
+resource "azurerm_resource_group" "example" {
+  name     = "resourceGroup1"
+  location = "West US"
+}
+
+resource "azurerm_iothub_dps" "example" {
+  name                = "__resourceiotdps__"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  sku {
+    name     = "__resourceiotdpsskuname__"
+    capacity = "__resourceiotdpsskucapacity__"
+  }
+}
+
+# IOT Device provisioning Service Certificate
+/*resource "azurerm_iothub_dps_certificate" "example" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.example.name
+  iot_dps_name        = azurerm_iothub_dps.example.name
+
+  certificate_content = filebase64("example.cer")
+}*/
+
+# Device Provisioning Service Shared Access policy
+/*resource "azurerm_iothub_dps_shared_access_policy" "example" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.example.name
+  iothub_dps_name     = azurerm_iothub_dps.example.name
+
+  enrollment_write = true
+  enrollment_read  = true
+}*/
