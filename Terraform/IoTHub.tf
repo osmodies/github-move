cb968c0d0a1a17a3bb5ba17c53765c641c6729f2
Oscar Díaz <odiaz@synergytpc.com>
2020-10-27 04:39:40 +0000

    Updated IoTHub.tf

--- Terraform/IoTHub.tf
+++ Terraform/IoTHub.tf
@@ -1,49 +1,49 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "resourcegropuiothub" {
   name     = "__resourcegroupname__"
   location = "__resourcegrouplocation__"
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "mystorage" {
   name                     = "__resourcestoragename__"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.resourcegropuiothub.name
+  location                 = azurerm_resource_group.resourcegropuiothub.location
   account_tier             = "__reourcestorageaccounttier__"
   account_replication_type = "__resourcestorgageaccountreplication__"
 }
 
-resource "azurerm_storage_container" "example" {
+resource "azurerm_storage_container" "mystoragecontainer" {
   name                  = "__resourcestoragecontainername__"
-  storage_account_name  = azurerm_storage_account.example.name
+  storage_account_name  = azurerm_storage_account.mystorage.name
   container_access_type = "__resourcesstoragecontaineraccess__"
 }
 
-resource "azurerm_eventhub_namespace" "example" {
+resource "azurerm_eventhub_namespace" "myeventhubnamespace" {
   name                = "__resourceeventhubnamepace__"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.resourcegropuiothub.name
+  location            = azurerm_resource_group.resourcegropuiothub.location
   sku                 = "__resourceeventhubnamespacesku__"
 }
 
-resource "azurerm_eventhub" "example" {
+resource "azurerm_eventhub" "myeventhub" {
   name                = "__resourceeventhubname__"
-  resource_group_name = azurerm_resource_group.example.name
-  namespace_name      = azurerm_eventhub_namespace.example.name
+  resource_group_name = azurerm_resource_group.resourcegropuiothub.name
+  namespace_name      = azurerm_eventhub_namespace.myeventhubnamespace.name
   partition_count     = __resourceeventhubpartitioncount__
   message_retention   = __resourceeventhubmessageretention__
 }
 
-resource "azurerm_eventhub_authorization_rule" "example" {
-  resource_group_name = azurerm_resource_group.example.name
-  namespace_name      = azurerm_eventhub_namespace.example.name
-  eventhub_name       = azurerm_eventhub.example.name
+resource "azurerm_eventhub_authorization_rule" "eventhubautorization" {
+  resource_group_name = azurerm_resource_group.resourcegropuiothub.name
+  namespace_name      = azurerm_eventhub_namespace.myeventhubnamespace.name
+  eventhub_name       = azurerm_eventhub.myeventhub.name
   name                = "__resourceauthorizationrulename__"
   send                = __resourceauthorizationrulesend__
 }
 
-resource "azurerm_iothub" "example" {
+resource "azurerm_iothub" "myitohub" {
   name                = "__resourceiothubname__"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.resourcegropuiothub.name
+  location            = azurerm_resource_group.resourcegropuiothub.location
 
   sku {
     name     = "S1"
@@ -52,18 +52,18 @@ resource "azurerm_iothub" "example" {
 
   endpoint {
     type                       = "AzureIotHub.StorageContainer"
-    connection_string          = azurerm_storage_account.example.primary_blob_connection_string
+    connection_string          = azurerm_storage_account.mystorage.primary_blob_connection_string
     name                       = "__routeendpoint__"
     batch_frequency_in_seconds = __endpointbatchfrecuency__
     max_chunk_size_in_bytes    = __endpointmaxchunksize__
-    container_name             = azurerm_storage_container.example.name
+    container_name             = azurerm_storage_container.mystoragecontainer.name
     encoding                   = "__endpointencoding__"
     file_name_format           = "__endpointfilenameformat__"
   }
 
   endpoint {
     type              = "AzureIotHub.EventHub"
-    connection_string = azurerm_eventhub_authorization_rule.example.primary_connection_string
+    connection_string = azurerm_eventhub_authorization_rule.eventhubautorization.primary_connection_string
     name              = "__routeendpoint2__"
   }
 
