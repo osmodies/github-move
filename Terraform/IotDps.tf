eb4f6e4b83a85cfb9810666494c4a54c34ef29c8
Oscar Díaz <odiaz@synergytpc.com>
2020-10-27 05:04:42 +0000

    Updated IotDps.tf

--- Terraform/IotDps.tf
+++ Terraform/IotDps.tf
@@ -1,9 +1,3 @@
-provider "azurerm" {
-    # The "feature" block is required for AzureRM provider 2.x. 
-    # If you're using version 1.x, the "features" block is not allowed.
-    version = "~>2.0"
-    features {}
-}
 resource "azurerm_resource_group" "resourcegroupiotdps" {
   name     = "__resourcegroupname__"
   location = "__resourcegrouplocation__"
