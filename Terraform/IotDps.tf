6226d4b28f2db90ceb7c2b8c5d295d51d1d5b57f
Oscar Díaz <odiaz@synergytpc.com>
2020-10-27 04:33:37 +0000

    Updated IotDps.tf

--- Terraform/IotDps.tf
+++ Terraform/IotDps.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "resourcegroupiotdps" {
   name     = "__resourcegroupname__"
   location = "__resourcegrouplocation__"
 }
 
-resource "azurerm_iothub_dps" "example" {
+resource "azurerm_iothub_dps" "iotdps" {
   name                = "__resourceiotdps__"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.resourcegroupiotdps.name
+  location            = azurerm_resource_group.resourcegroupiotdps.location
 
   sku {
     name     = "__resourceiotdpsskuname__"
@@ -15,19 +15,19 @@ resource "azurerm_iothub_dps" "example" {
 }
 
 # IOT Device provisioning Service Certificate
-/*resource "azurerm_iothub_dps_certificate" "example" {
+/*resource "azurerm_iothub_dps_certificate" "dpscertificate" {
   name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  iot_dps_name        = azurerm_iothub_dps.example.name
+  resource_group_name = azurerm_resource_group.resourcegroupiotdps.name
+  iot_dps_name        = azurerm_iothub_dps.iotdps.name
 
   certificate_content = filebase64("example.cer")
 }*/
 
 # Device Provisioning Service Shared Access policy
-/*resource "azurerm_iothub_dps_shared_access_policy" "example" {
+/*resource "azurerm_iothub_dps_shared_access_policy" "dpsaccesspolicy" {
   name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  iothub_dps_name     = azurerm_iothub_dps.example.name
+  resource_group_name = azurerm_resource_group.resourcegroupiotdps.name
+  iothub_dps_name     = azurerm_iothub_dps.iotdps.name
 
   enrollment_write = true
   enrollment_read  = true
