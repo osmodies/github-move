5987b47e6a9160952f7299b976f363f3d29d7f2a
Oscar Díaz <odiaz@synergytpc.com>
2020-10-27 02:56:27 +0000

    Updated StorageAccount.tf

--- Terraform/StorageAccount.tf
+++ Terraform/StorageAccount.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "StorageAccount" {
+resource "azurerm_resource_group" "resourcegroup" {
   name     = "__resourcegroupname__"
   location = "__resourcegrouplocation__"
 }
 
-resource "azurerm_storage_account" "StorageAccount" {
+resource "azurerm_storage_account" "mystorageaccount" {
   name                     = "__storageaccountname__"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.resourcegroup.name
+  location                 = azurerm_resource_group.resourcegroup.location
   account_tier             = "__account_tier__"
   account_replication_type = "__account_replication_type__"
 
