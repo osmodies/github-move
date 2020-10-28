5c0a76ef500d3d555206bcba1e75314e5d53e055
Oscar Díaz <odiaz@synergytpc.com>
2020-10-28 21:05:06 +0000

    Updated VMLinux.tf

--- Terraform/VMLinux.tf
+++ Terraform/VMLinux.tf
@@ -94,11 +94,11 @@ resource "azurerm_linux_virtual_machine" "myvm" {
         storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
     }
 
-  tags {​​
-    "Fecha de Creacion en la Nube" = "__var.creation__"
-    "Contacto_Infraestructura" = "__var.contact__"
-    "Contacto_Solucion" = "__var.contactSolution__"
-    "Servicio-Aplicacion" = "__var.app__"
-    "Descripcion" = "__var.description__"
- }​​
+  tags = {​​
+    Fecha_de_Creacion_en_la_Nube = "__var.creation__"
+    Contacto_Infraestructura = "__var.contact__"
+    Contacto_Solucion = "__var.contactSolution__"
+    Servicio-Aplicacion = "__var.app__"
+    Descripcion = "__var.description__"
+ }​
 }
