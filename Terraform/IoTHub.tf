7a915f2582a13aaeb91d57382b90177c1c3a6483
Oscar Díaz <odiaz@synergytpc.com>
2020-10-28 19:17:03 +0000

    Updated IoTHub.tf

--- Terraform/IoTHub.tf
+++ Terraform/IoTHub.tf
@@ -86,11 +86,10 @@ resource "azurerm_iothub" "myitohub" {
 
   tags = {​​
 
-    "Fecha de Creacion en la Nube" = "__var.var.creation__"
-    "Contacto_Infraestructura" = "__var.var.contact__"
-    "Contacto_Solucion" = "__var.var.contactSolution__"
-    "Servicio-Aplicacion" = "__var.var.app__"
-    "Descripcion" = "__var.var.description__"
- }​​
-  
+    "Fecha de Creacion en la Nube" = "__var.creation__"
+    "Contacto_Infraestructura" = "__var.contact__"
+    "Contacto_Solucion" = "__var.contactSolution__"
+    "Servicio-Aplicacion" = "__var.app__"
+    "Descripcion" = "__var.description__"
+ }​​ 
 }
