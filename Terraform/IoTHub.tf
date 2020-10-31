11b7a4edaa1e90952c6611034c3b6030211203ec
Oscar Díaz <odiaz@synergytpc.com>
2020-10-31 21:30:22 +0000

    Updated IoTHub.tf

--- Terraform/IoTHub.tf
+++ Terraform/IoTHub.tf
@@ -1,5 +1,5 @@
 terraform {  
-backend "azurerm" {
+  backend "azurerm" {
     storage_account_name = "__var.terraformstorageaccount__"
     container_name       = "__var.terraformContainername__"
     key                  = "__var.terraformstoragekey__"
